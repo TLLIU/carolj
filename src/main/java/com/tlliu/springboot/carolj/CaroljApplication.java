cffb3dc8e4acbdbd260c0405003286bb924ed05e
Tailei Liu <liutailei@gmail.com>
2021-05-28 15:56:47 -0500

    Import default generated projects from sprint.io

---
+++ src/main/java/com/tlliu/springboot/carolj/CaroljApplication.java
@@ -0,0 +1,13 @@
+package com.tlliu.springboot.carolj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaroljApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaroljApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tlliu/springboot/carolj/CaroljApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tlliu.springboot.carolj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaroljApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
